30aec24607840d98061b7c9a6f7518cfcc22869a
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-03-05 01:14:09 +0900

    tmp react icons to svg

--- src/client/components/foundation/Icon/Icon.tsx
+++ src/client/components/foundation/Icon/Icon.tsx
@@ -1,21 +1,43 @@
+//tmp
+import { dom, library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faArrowLeft,
+  faArrowRight,
+  faCheckCircle,
+  faPlay,
+  faShoppingCart,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames';
 import type { FC } from 'react';
-import * as Icons from 'react-icons/fa';
 
 import * as styles from './Icon.styles';
 
+library.add(faShoppingCart, faArrowLeft, faArrowRight, faUser, faPlay, faCheckCircle);
+dom.watch();
+type Icons = 'FaShoppingCart' | 'FaArrowLeft' | 'FaArrowRight' | 'FaUser' | 'FaPlay' | 'FaCheckCircle';
+
+// パスカルケースをケバブケースにする
+const PascalToKebab = (pascal: string) => {
+  console.log(PascalToKebab);
+  return pascal
+    .replace(/[A-Z]/g, (searchStr: string) => {
+      return '-' + searchStr.charAt(0).toLowerCase();
+    })
+    .slice(1);
+};
+
 type Props = {
-  type: keyof typeof Icons;
+  type: Icons;
   width: number;
   height: number;
   color: string;
 };
 
 export const Icon: FC<Props> = ({ color, height, type, width }) => {
-  const Icon = Icons[type];
   return (
-    <span className={classNames(type, styles.container({ color, height, width }))}>
-      <Icon />
+    <span className={classNames(styles.container({ color, height, width }))}>
+      <i className={classNames('fas', PascalToKebab(type))} />
     </span>
   );
 };
