b75f21bf95525a3db6b1d5649e9c2762ddbe683f
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-03-05 14:13:44 +0900

    install koa compress

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,6 +4,7 @@ import { koaMiddleware } from '@as-integrations/koa';
 import gracefulShutdown from 'http-graceful-shutdown';
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import compress from 'koa-compress';
 import logger from 'koa-logger';
 import route from 'koa-route';
 import send from 'koa-send';
@@ -40,6 +41,7 @@ async function init(): Promise<void> {
   app.keys = ['cookie-key'];
   app.use(logger());
   app.use(bodyParser());
+  app.use(compress())
   app.use(session({}, app));
 
   app.use(async (ctx, next) => {
